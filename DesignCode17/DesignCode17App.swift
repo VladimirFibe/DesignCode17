1345b4c3cdcb2437d12914d8718f7e485334c232
Vladimir Fibe <motiw@icloud.com>
2023-10-31 11:30:28 +0600

    Initial Commit

---
+++ DesignCode17/DesignCode17App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode17App.swift
+//  DesignCode17
+//
+//  Created by Vladimir Fibe on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
