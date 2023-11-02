1efa4748b657ccdd2d2dae0d3d34420e4f44778a
Vladimir Fibe <motiw@icloud.com>
2023-11-02 16:34:09 +0600

    save

--- DesignCode17/ContentView.swift
+++ DesignCode17/ContentView.swift
@@ -3,12 +3,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "sparkles")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Size")
+                        .foregroundStyle(.secondary)
+                    Text("1024x1024")
+                }
+                Divider()
+                VStack(alignment: .leading) {
+                    Text("Type")
+                        .foregroundStyle(.secondary)
+                    Text("Upscale")
+                }
+                Divider()
+                VStack(alignment: .leading) {
+                    Text("Date")
+                        .foregroundStyle(.secondary)
+                    Text("Today 5:19")
+                }
+            }
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .frame(height: 44)
         }
         .padding()
+        .background(.green.gradient)
+        .cornerRadius(20)
     }
 }
 
